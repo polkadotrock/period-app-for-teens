66eb4e4f366f91de11476b16303c6e923651668e
jennienguyendev <jennienguyendev@gmail.com>
2023-10-14 18:07:36 -0700

    edited instructions in index.js

--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,12 @@ router.get('/', (req, res) => {
     res.render('dashboard');
 });
 
-//------ TEST YOUR EJS FILE HERE ------//
+/*
+------ TEST YOUR EJS FILE HERE ------
 
-// replace '/test' and 'test' with the name of your file for testing purposes
+replace '/test' and 'test' with the name of your file for testing purposes
+in browser, go to http://localhost:3000/test (replace test with the name of your ejs file) to view
+*/
 
 router.get('/test', (req, res) => {
     res.render('test');
