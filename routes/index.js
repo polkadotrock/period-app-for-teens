49582a8ed81e13f3e202afa3b429da9ead36aefd
Aazeen <aazeen@gmail.com>
2023-10-16 19:20:42 -0400

    add first article

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // desc     dashboard
-// route    GET / 
-router.get('/', (req, res) => {
-    res.render('dashboard');
+// route    GET /
+router.get("/", (req, res) => {
+  res.render("dashboard");
 });
 
 /*
@@ -13,8 +13,8 @@ router.get('/', (req, res) => {
 replace '/test' and 'test' with the name of your file for testing purposes
 in browser, go to http://localhost:3000/test (replace 'test' with the name of your ejs file) to view
 */
-router.get('/test', (req, res) => {
-    res.render('test');
+router.get("/learn", (req, res) => {
+  res.render("learn");
 });
 
 module.exports = router;
