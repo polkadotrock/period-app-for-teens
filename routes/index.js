62d1a7460e16ef609940b3e19ffb2f769c838686
jennienguyendev <jennienguyendev@gmail.com>
2023-10-21 12:02:28 -0700

    added delete log

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,19 @@ const Passage = require("@passageidentity/passage-node");
 const dotenv = require("dotenv");
 const router = express.Router();
 
+const Log = require("../models/Log");
+
 dotenv.config({ path: "./config/config.env" });
 
 // Passage configuration
 const passageConfig = {
   appID: process.env.PASSAGE_APP_ID,
   apiKey: process.env.PASSAGE_API_KEY,
+  // authStrategy: "HEADER",
 };
 
 const passage = new Passage(passageConfig); // initialize Passage
+
 
 // Custom Passage middleware
 const passageAuthMiddleware = async (req, res, next) => {
@@ -22,60 +26,101 @@ const passageAuthMiddleware = async (req, res, next) => {
       res.userID = userID;
       next();
     }
-  } catch (e) {
-    console.log(e);
+  } catch(err) {
+    console.error(err);
     res.render("unauthorized.ejs"); // render the unauthorized.ejs file if the user is not authenticated
   }
 };
 
 // Login route
-router.get("/login", (req, res) => {
-  res.render("login.ejs", { appID: passageConfig.appID });
+router.get("/login", async (req, res) => {
+  try {
+    res.render("login.ejs", { appID: passageConfig.appID });
+  } catch (err) {
+    console.error(err);
+    res.redirect('/');
+  }
+  
 });
 
 // Authenticated route that uses the Passage middleware
 router.get("/dashboard", passageAuthMiddleware, async (req, res) => {
-  let userID = res.userID;
-  let user = await passage.user.get(userID);
+  try {
+    let userID = res.userID;
+    let user = await passage.user.get(userID);
 
-  let userIdentifier;
-  if (user.email) {
-    userIdentifier = user.email;
-  } else if (user.phone) {
-    userIdentifier = user.phone;
-  }
+    let userIdentifier;
+    if (user.email) {
+      userIdentifier = user.email;
+    } else if (user.phone) {
+      userIdentifier = user.phone;
+    }
 
-  res.render("dashboard.ejs", { userIdentifier });
+    res.render("dashboard.ejs", { userIdentifier });
+  } catch (err) {
+    console.error(err);
+    res.render('dashboard-guest');
+  }  
 });
 
 // Logout route
-router.get("/logout", (req, res) => {
+router.get("/logout", async (req, res) => {
+try {
   res.clearCookie("passage-session");
   res.redirect("/");
+} catch (err) {
+  console.error(err);
+    res.redirect('/');
+}
 });
 
 // Profile route
 router.get("/profile", passageAuthMiddleware, async (req, res) => {
-  const userID = res.userID;
-  const user = await passage.user.get(userID);
-  const appID = passageConfig.appID;
 
-  res.render("profile.ejs", { user, appID });
+  try {
+    const userID = res.userID;
+    const user = await passage.user.get(userID);
+    const appID = passageConfig.appID;
+
+    res.render("profile", { user, appID });
+  } catch (err) {
+    console.error(err);
+    res.redirect('/');
+  }
+  
+});
+
+// GET /
+router.get('/', passageAuthMiddleware, async (req, res) => {
+  try {
+      res.render('dashboard');
+    
+  } catch (err) {
+    console.error(err);
+        res.redirect('/guest');
+  }
 });
 
 // GET /guest
-router.get('/guest', (req, res) => {
-    res.render('dashboard-guest');
+router.get('/guest', async (req, res) => {
+  try {
+      res.render('dashboard-guest');
+    
+  } catch (err) {
+    console.error(err);
+        res.redirect('/');
+  }
 });
 
 // GET /learn
-router.get('/learn', (req, res) => {
-    res.render('learn');
-});
+router.get('/learn', async (req, res) => {
+    try {
 
-// GET /learn-guest
-router.get('/learn-guest', (req, res) => {
-    res.render('learn-guest');
+        res.render('learn');
+    } catch (err) {
+        console.error(err);
+        res.redirect('/');
+    }
 });
 
 // POST /article
@@ -89,25 +134,75 @@ router.post('/article', async (req, res) => {
 });
 
 // GET /article
-router.get('/article', (req, res) => {
-    res.render('article');
+router.get('/article', async (req, res) => {
+    try {
+      res.render('article');
+    } catch (err) {
+      console.error(err);
+      res.redirect('/');
+    }    
 });
 
-// GET /user
-router.get('/user', (req, res) => {
-    res.render('user');
-});
+// // GET /user
+// router.get('/user', passageAuthMiddleware, async (req, res) => {
+//     try {
+//       res.render('profile');
+//     } catch (err) {
+//       console.error(err);
+//       res.redirect('/');
+//     }    
+// });
 
 // GET /calendar
-router.get('/calendar', passageAuthMiddleware, (req, res) => {
-    // console.log('res.userid from calendar ', res.userID);
+router.get('/calendar', passageAuthMiddleware, async (req, res) => {
+    try {
+      // console.log('req body from dashboard ', req.res.userID);
 
-    res.render('calendar', { user: res.userID });
+      const pastCycles = await Log.find({ user: res.userID })
+        .sort({year: -1, month: -1})
+        .lean();
+      // console.log(pastCycles);
+      res.render('calendar', { pastCycles });
+    } catch (err) {
+      console.error(err);
+      res.redirect('/');
+    }
 });
 
 // POST /logcycle
-router.post('/logcycle', passageAuthMiddleware, (req, res) => {
-    console.log('req.body: ', req.body);
+router.post('/logcycle', passageAuthMiddleware, async (req, res) => {
+    try {
+      // console.log('req.res.userID ', req.res.userID);
+      // console.log('req.body: ', req.body);
+      const days = req.body.days.filter(e => e);
+      const month = req.body.month;
+      const year = req.body.year;
+      // console.log('days', days);
+      // console.log(month);
+      // console.log(year);
+
+      const log = await Log.create({
+        days: days,
+        month: month,
+        year: year,
+        user: req.res.userID,
+      });
+      res.redirect('/calendar');
+    } catch (err) {
+      console.error(err);
+      res.redirect('/dashboard');
+    }
+});
+
+// DELETE /:id
+router.delete('/:id',passageAuthMiddleware, async (req, res) => {
+  try {
+    await Log.deleteOne({ _id: req.params.id });
+      res.redirect('/calendar');
+  } catch (err) {
+    console.error(err);
+    return res.render('calendar');
+  }
 });
 
 module.exports = router;
